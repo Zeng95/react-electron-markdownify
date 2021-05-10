29ec1bf569c94e60a2de07b6dac9abe754d148af
Leo Zeng <qingchun1995@gmail.com>
2021-05-10 22:33:56 +0800

    add: custom hook useKeyPress

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -1,9 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-function useKeyPress() {
-  const [orders, setOrders] = useState([])
+function useKeyPress(targetKeyCode) {
+  const [keyPressed, setKeyPressed] = useState(false)
 
-  return { orders, setOrders }
+  function handleKeyDown(event) {
+    const { keyCode } = event
+
+    if (keyCode === targetKeyCode) {
+      setKeyPressed(true)
+    }
+  }
+
+  function handleKeyUp(event) {
+    const { keyCode } = event
+
+    if (keyCode === targetKeyCode) {
+      setKeyPressed(false)
+    }
+  }
+
+  useEffect(() => {
+    const cleanup = () => {
+      document.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keyup', handleKeyUp)
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keyup', handleKeyUp)
+
+    return cleanup
+  })
+
+  return keyPressed
 }
 
 export default useKeyPress
